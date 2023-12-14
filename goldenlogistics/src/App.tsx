89d93fcca6ec17f18d70a4bb8d4e3c2089c0233e
Relativiteit <alejocain@gmail.com>
2023-12-14 22:16:02 +0100

    added 5 star rating readonly ;)

--- goldenlogistics/src/App.tsx
+++ goldenlogistics/src/App.tsx
@@ -5,7 +5,7 @@ import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
 import goldenlogisticsImage from './assets/goldenlogistics.jpeg'; // Use import to load the image
-import { Button, Modal } from '@mui/material';
+import { Button, Modal, Rating } from '@mui/material';
 function Copyright() {
 
   return (
@@ -35,7 +35,7 @@ const [isPhoneModalOpen, setPhoneModalOpen] = React.useState(false);
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>      <ResponsiveAppBar/>
       <Box sx={{ my: 4, textAlign: 'center'}} >
         <Typography variant="h4" component="h1" gutterBottom align='center'>
-          <h4>Golden Logistics, wij helpen u met al uw vervoer wensen!</h4>
+          <h4>Golden Logistics, wij helpen u graag logistieke wensen!</h4>
        </Typography>
        <img
           src={goldenlogisticsImage}
@@ -62,6 +62,10 @@ const [isPhoneModalOpen, setPhoneModalOpen] = React.useState(false);
             </Typography>
           </Box>
         </Modal>
+
+          <Typography component="legend"> Klant tevredenheid</Typography>\
+          <Rating name="read-only" defaultValue={5}  readOnly/>
+        
        <ProTip  />
         <Copyright />
       </Box>
