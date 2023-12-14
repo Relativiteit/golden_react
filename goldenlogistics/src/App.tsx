39eb1a85ec48b13ed2d8c469d0734d7a34f6adc7
Relativiteit <alejocain@gmail.com>
2023-12-14 22:01:40 +0100

    added modal and contact button

--- goldenlogistics/src/App.tsx
+++ goldenlogistics/src/App.tsx
@@ -5,8 +5,9 @@ import Link from '@mui/material/Link';
 import ProTip from './ProTip';
 import ResponsiveAppBar from './components/ResponsiveAppBar';
 import goldenlogisticsImage from './assets/goldenlogistics.jpeg'; // Use import to load the image
-
+import { Button, Modal } from '@mui/material';
 function Copyright() {
+
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
@@ -19,9 +20,20 @@ function Copyright() {
 }
 
 export default function App() {
+const [isPhoneModalOpen, setPhoneModalOpen] = React.useState(false);
+
+
+ const showPhoneModal = () => {
+  setPhoneModalOpen(true);
+ };
+
+ const hidePhoneModal = () => {
+  setPhoneModalOpen(false);
+ };
+
   return (
     <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', minHeight: '100vh' }}>      <ResponsiveAppBar/>
-      <Box sx={{ my: 4 }}>
+      <Box sx={{ my: 4, textAlign: 'center'}} >
         <Typography variant="h4" component="h1" gutterBottom align='center'>
           <h4>Golden Logistics, wij helpen u met al uw vervoer wensen!</h4>
        </Typography>
@@ -35,7 +47,21 @@ export default function App() {
             maxWidth: '100%',
             height: 'auto',
           }}
-        /> {/* Use the imported image */}        
+        /> 
+        <Button variant="contained" sx={{ fontSize: '3rem', padding: '15px 30px', marginTop: -130 }} onClick={showPhoneModal} >
+          Neem Contact op per Telefoon!
+          </Button>  
+
+          <Modal open={isPhoneModalOpen} onClose={hidePhoneModal}>
+          <Box sx={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', width: 400, bgcolor: 'background.paper', border: '2px solid #000', p: 4 }}>
+            <Typography variant="h6" component="h2" gutterBottom>
+              Contact Information
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Phone: +1234567890 {/* Replace this with your client's phone number */}
+            </Typography>
+          </Box>
+        </Modal>
        <ProTip  />
         <Copyright />
       </Box>
